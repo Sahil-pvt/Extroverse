8a8743e2396794387fa9e7f3ea622d0c8518a2bf
EMPEROR <sahilsorte8457@gmail.com>
2023-11-10 15:59:33 +0530

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './globals.css'
+import SigninForm from './_auth/forms/SigninForm'
+
+import SignupForm from './_auth/forms/SignupForm'
+import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello Extroverse
-        </h1>
+        <main className='flex h-screen'>
+            <Routes>
+                {/*public routes*/}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-up' element={<SignupForm />} />
+                </Route>
+
+
+                {/*private routes*/}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+
+            </Routes>
+        </main>
     )
 }
 
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,11 @@
+
+
+const RootLayout = () => {
+  return (
+    <div>
+      RootLayout
+    </div>
+  )
+}
+
+export default RootLayout
