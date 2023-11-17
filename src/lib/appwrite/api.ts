25cf3c6be2cfda7297d5a0d1bef2cad5d732ab7e
EMPEROR <sahilsorte8457@gmail.com>
2023-11-17 17:24:17 +0530

    Post card with like and save post functionality

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -144,7 +144,7 @@ export async function uploadFile(file: File) {
     }
 }
 
-export async function getFilePreview(fileId: string) {
+export function getFilePreview(fileId: string) {
     try {
         const fileUrl = storage.getFilePreview(
             appwriteConfig.storageId,
@@ -166,6 +166,73 @@ export async function deleteFile(fileId: string) {
         await storage.deleteFile(appwriteConfig.storageId, fileId);
 
         return { status: 'ok' }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getRecentPosts() {
+    const posts = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        [Query.orderDesc('$createdAt'), Query.limit(20)]
+    )
+
+    if(!posts) throw Error;
+
+    return posts;
+}
+
+export async function likePost(postId: string, likesArray: string[]) {
+    try {
+        const updatedPost = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            postId,
+            {
+                likes: likesArray
+            }
+        )
+
+        if(!updatedPost) throw Error;
+
+        return updatedPost;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function savePost(postId: string, userId: string) {
+    try {
+        const updatedPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionId,
+            ID.unique(),
+            {
+                user: userId,
+                post: postId,
+            }
+        )
+
+        if(!updatedPost) throw Error;
+
+        return updatedPost;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function deleteSavedPost(savedRecordId: string) {
+    try {
+        const statusCode = await databases.deleteDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionId,
+            savedRecordId
+        )
+
+        if(!statusCode) throw Error;
+
+        return {status: 'ok'};
     } catch (error) {
         console.log(error)
     }
