9be85bb73f0a48a4774c20813c7ae01394c098b9
EMPEROR <sahilsorte8457@gmail.com>
2023-11-21 00:16:12 +0530

    Explore page with search post functionality

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -5,16 +5,20 @@ import { Models } from "appwrite";
 
 
 const Home = () => {
-  const {data: posts, isPending: isPostLoading, isError: isErrorPosts} = useGetRecentPosts();
+  const { data: posts, isPending: isPostLoading, isError: isErrorPosts } = useGetRecentPosts();
   return (
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? (<Loader />) : (
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
             <ul className="flex flex-col flex-1 gap-9 w-full">
               {posts?.documents.map((post: Models.Document) => (
-                <PostCard post={post} key={post.caption} />
+                <li key={post.$id} className="flex justify-center w-full">
+                  <PostCard post={post} />
+                </li>
               ))}
             </ul>
           )}
